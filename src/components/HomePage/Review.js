fbfdd51e546fe6cde1a2f9b56f5d9a713c69e9cd
arifa-akter <arifaakterchoity@gmail.com>
2022-05-23 23:02:36 +0600

    add review part

--- src/components/HomePage/Review.js
+++ src/components/HomePage/Review.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import ReviewRow from './ReviewRow';
 
 const Review = () => {
+    const [reviews , setReview] = useState([])
+     useEffect(()=>{
+          fetch('http://localhost:5000/review')
+          .then(res=>res.json())
+          .then(data=>setReview(data))
+     } ,[])
     return (
-     <section className=" lg:mb-28">
+     <section className=" lg:mb-28 mb-11">
          <div className="container mx-auto">
              <h1 className="text-center font-bold lg:text-5xl
               text-2xl text-accent">Customer Review</h1>
-              <p className='text-center text-secondary text-xl pt-2'>Here you can see our customer review</p>
+              <p className='text-center text-secondary text-xl pt-2 mt-5 mb-11'>Here you can see our customer review</p>
 
-              <div className='grid grid-cols-1 lg:grid-cols-3'>
-                  
+              <div className='grid grid-cols-1 lg:grid-cols-3 gap-5'>
+                  {
+                    reviews.map(review=><ReviewRow 
+                        key={review._id}
+                        review={review}
+                    ></ReviewRow>)
+                  }
               </div>
          </div>
          
--- src/components/HomePage/ReviewRow.js
+++ src/components/HomePage/ReviewRow.js
@@ -1,10 +1,47 @@
 import React from 'react';
 
-const ReviewRow = () => {
+const ReviewRow = ({review}) => {
+    const{name , image , rating , description} = review
     return (
-        <div>
-            
+        <div className="card bg-base-100 shadow-xl m-2">
+        <figure className="px-10 pt-10">
+          <img src={image} alt="Shoes" className="rounded-xl bg-primary w-[80px] h-[80px]" />
+        </figure>
+        <div className="card-body items-center text-center">
+          <h2 className="card-title text-primary">{name}</h2>
+          <p> {rating === '4' &&
+          <>
+          <i className="fa fa-star text-secondary" aria-hidden="true"></i>
+          <i className="fa fa-star text-amber-500" aria-hidden="true"></i>
+          <i className="fa fa-star text-secondary" aria-hidden="true"></i>
+          <i className="fa fa-star text-amber-500" aria-hidden="true"></i>
+          </>
+          }
+          {rating === '5' &&
+              <>
+              <i className="fa fa-star text-secondary" aria-hidden="true"></i>
+              <i className="fa fa-star text-amber-500" aria-hidden="true"></i>
+              <i className="fa fa-star text-secondary" aria-hidden="true"></i>
+              <i className="fa fa-star text-amber-500" aria-hidden="true"></i>
+              <i className="fa fa-star text-secondary" aria-hidden="true"></i>
+              </>
+          }
+     
+          {rating === '3' &&  <>
+          <i className="fa fa-star text-secondary" aria-hidden="true"></i>
+          <i className="fa fa-star text-amber-500" aria-hidden="true"></i>
+          <i className="fa fa-star text-secondary" aria-hidden="true"></i>
+          </>}
+          {rating === '2' && <>
+          <i className="fa fa-star text-secondary" aria-hidden="true"></i>
+          <i className="fa fa-star text-amber-500" aria-hidden="true"></i>
+          </>}
+          {rating === '1' &&<i className="fa fa-star text-amber-500" aria-hidden="true"></i>}
+          {rating}</p>
+
+          <p>{description}</p>
         </div>
+      </div>
     );
 };
 
