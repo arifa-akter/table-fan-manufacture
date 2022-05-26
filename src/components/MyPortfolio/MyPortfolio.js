894a825f46ed9ca1b3bc2106d4dfca553c2800a3
arifa-akter <arifaakterchoity@gmail.com>
2022-05-26 11:27:59 +0600

    add link

--- src/components/MyPortfolio/MyPortfolio.js
+++ src/components/MyPortfolio/MyPortfolio.js
@@ -18,9 +18,9 @@ const MyPortfolio = () => {
                         <span className='font-bold lg:text-xl'>Bsc computer science and engineering from National university</span></p>
                         <p className="mb-5"><span className='font-bold lg:text-xl text-secondary pr-3'>Technology:</span>
                         <span className='font-bold lg:text-xl'>I know about bootstrap, tailwind, react.js node.js mongodb </span></p>
-                        <p className="mb-5"><span className='font-bold lg:text-xl text-secondary pr-3'>link :<span className='text-accent'>websiteLink</span></span>
+                        <p className="mb-5"><span className='font-bold lg:text-xl text-secondary pr-3'>link :<span className='text-accent'>websiteLink</span>  https://wild-photographer-1fd87.web.app/</span>
                         </p>
-                        <p className="mb-5"><span className='font-bold lg:text-xl text-secondary pr-3'>link :<span className='text-accent'>websiteLink</span></span>
+                        <p className="mb-5"><span className='font-bold lg:text-xl text-secondary pr-3'>link :<span className='text-accent'>websiteLink</span> https://car-inventory-613c5.web.app/</span>
                         </p>
                         <p className="mb-5"><span className='font-bold lg:text-xl text-secondary pr-3'>link :<span className='text-accent'>websiteLink</span></span>
                         </p>
